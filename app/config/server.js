8cbd003c8b4998372014604df4a6ed6cc0e5fdc0
rajaaltus <rajaaltus@gmail.com>
2022-01-10 22:04:06 +0700

    Server config small update

--- app/config/server.js
+++ app/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: "http://c8c1ece66b72.ngrok.io",
+  url: "https://api2.rotihouselao.com",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "166efc337cab41c074765f86f9bb25a2"),
