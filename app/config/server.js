e15f129e50895b49006c928853546450af560c97
iROW9 <irow9.ks@gmail.com>
2024-12-09 11:53:42 +0700

    updated

--- app/config/server.js
+++ app/config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://api2.rotihouselao.com",
+  // url: "https://api2.rotihouselao.com",
+  url: "http://192.168.0.12",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "166efc337cab41c074765f86f9bb25a2"),
